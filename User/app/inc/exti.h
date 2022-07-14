22b9030e6bb2d76c329eb22282407edffd09cb6f
ThisIsMarxMA <ma376371893@163.com>
2022-07-14 15:17:19 +0800

    revert output level

--- User/app/inc/exti.h
+++ User/app/inc/exti.h
@@ -25,26 +25,26 @@
 #define OBST_DETC       GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11)
 #define DOOR_DETC       GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_12)
 
-#define EMRLIGHT_OFF			GPIO_SetBits(GPIOF,GPIO_Pin_0)		  
-#define EMRLIGHT_ON		    GPIO_ResetBits(GPIOF,GPIO_Pin_0)			//RED
+#define EMRLIGHT_ON				GPIO_SetBits(GPIOF,GPIO_Pin_0)		  
+#define EMRLIGHT_OFF	    GPIO_ResetBits(GPIOF,GPIO_Pin_0)			//RED
 
-#define RUNLIGHT_OFF		  GPIO_SetBits(GPIOF,GPIO_Pin_1)	
-#define RUNLIGHT_ON		    GPIO_ResetBits(GPIOF,GPIO_Pin_1)			//GREEN
+#define RUNLIGHT_ON			  GPIO_SetBits(GPIOF,GPIO_Pin_1)	
+#define RUNLIGHT_OFF	    GPIO_ResetBits(GPIOF,GPIO_Pin_1)			//GREEN
 
-#define TEMPSTOP_OFF	  	GPIO_SetBits(GPIOF,GPIO_Pin_2)		
-#define TEMPSTOP_ON	      GPIO_ResetBits(GPIOF,GPIO_Pin_2)			//YELLOW 
+#define TEMPSTOP_ON	  	  GPIO_SetBits(GPIOF,GPIO_Pin_2)		
+#define TEMPSTOP_OFF	    GPIO_ResetBits(GPIOF,GPIO_Pin_2)			//YELLOW 
 
-#define Z1BRAKE_CLOSE     GPIO_SetBits(GPIOB,GPIO_Pin_5)
-#define Z1BRAKE_RELEASE   GPIO_ResetBits(GPIOB,GPIO_Pin_5) 
+#define Z1BRAKE_RELEASE   GPIO_SetBits(GPIOB,GPIO_Pin_5)
+#define Z1BRAKE_CLOSE     GPIO_ResetBits(GPIOB,GPIO_Pin_5) 
 
-#define Z2BRAKE_CLOSE     GPIO_SetBits(GPIOB,GPIO_Pin_6)
-#define Z2BRAKE_RELEASE   GPIO_ResetBits(GPIOB,GPIO_Pin_6) 
+#define Z2BRAKE_RELEASE   GPIO_SetBits(GPIOB,GPIO_Pin_6)
+#define Z2BRAKE_CLOSE     GPIO_ResetBits(GPIOB,GPIO_Pin_6) 
 
-#define Z3BRAKE_CLOSE     GPIO_SetBits(GPIOB,GPIO_Pin_7)
-#define Z3BRAKE_RELEASE   GPIO_ResetBits(GPIOB,GPIO_Pin_7) 
+#define Z3BRAKE_RELEASE   GPIO_SetBits(GPIOB,GPIO_Pin_7)
+#define Z3BRAKE_CLOSE     GPIO_ResetBits(GPIOB,GPIO_Pin_7) 
 
-#define YBRAKE_CLOSE      GPIO_SetBits(GPIOB,GPIO_Pin_8)
-#define YBRAKE_RELEASE    GPIO_ResetBits(GPIOB,GPIO_Pin_8) 
+#define YBRAKE_RELEASE    GPIO_SetBits(GPIOB,GPIO_Pin_8)
+#define YBRAKE_CLOSE      GPIO_ResetBits(GPIOB,GPIO_Pin_8) 
 
 
 void GPIO_Setup(void);	//IO≥ı ºªØ
